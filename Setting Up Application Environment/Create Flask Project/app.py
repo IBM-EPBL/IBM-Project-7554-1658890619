16c383daadd6eea9e2e5a6b67bede78f3362fc14
Mugesh1817 <113584851+Mugesh1817@users.noreply.github.com>
2022-11-19 13:47:09 +0530

    Add files via upload

---
+++ Setting Up Application Environment/Create Flask Project/app.py
@@ -0,0 +1,66 @@
+# from flask import Flask,render_template
+# app= Flask(__name__)
+# @app.route("/")
+# def Home():
+#     return render_template('Home.html')
+# @app.route("/about")
+# def About():
+#     return render_template('about.html')
+# @app.route("/signup")
+# def signup():
+#     return render_template('signup.html')
+# @app.route("/signin")
+# def signin():
+#     return render_template('signin.html')
+    
+ 
+from flask import Flask, flash, request, redirect, url_for, render_template
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
+ 
+app = Flask(__name__)
+ 
+UPLOAD_FOLDER = 'static/uploads/'
+ 
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+     
+ 
+@app.route('/')
+def Home():
+    return render_template('Home.html')
+ 
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('index.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+ 
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+ 
+if __name__ == "__main__":
+    app.run()
+ 
